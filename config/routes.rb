0fbb8e2b164aa846026ad05e0c650cd27fd2dd0d
edgkid <edglandaeta.15@gmail.com>
2019-08-02 00:16:46 +0200

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  #root to: 'pages#index'
+  root to: 'pages#index'
   devise_for :users
   resources :users
-  get 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root to: 'users#index'
 
