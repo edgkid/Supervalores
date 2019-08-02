15374211140e278f8905ca3c156b26a46a1e1802
Jesús Di Bari <iijbari@gmail.com>
2019-08-01 20:58:23 -0400

    commit

--- app/models/t_recibo.rb
+++ app/models/t_recibo.rb
@@ -1,16 +1,16 @@
 class TRecibo < ApplicationRecord
 
-	blongs_to :t_factura
-    blongs_to :t_cliente
-    blongs_to :t_periodo
-    blongs_to :t_metodo_pago
-    blongs_to :user
+	belongs_to :t_factura
+  belongs_to :t_cliente
+  belongs_to :t_periodo
+  belongs_to :t_metodo_pago
+  belongs_to :user
 
-    #has_many :t_recibo_detalle
-    has_many :t_caja
-    has_many :t_nota_credito
+  #has_many :t_recibo_detalle
+  has_many :t_caja
+  has_many :t_nota_credito
 
-    has_many :t_estado_cuentum
-    has_many :t_cliente, through: :t_estado_cuentum
+  has_many :t_estado_cuentum
+  has_many :t_cliente, through: :t_estado_cuentum
     
 end
