dc2a23e8d0a0409a5907873207f649850f9e77fb
edgkid <edglandaeta.15@gmail.com>
2019-08-04 12:46:34 +0200

    index rol
    Integracion de index rol completa

--- app/controllers/t_rols_controller.rb
+++ app/controllers/t_rols_controller.rb
@@ -1,22 +1,36 @@
 class TRolsController < ApplicationController
-  def index,
+
+  before_action :set_t_rol, only: [:edit, :update, :destroy]
+
+  def index
+    @rols = TRol.all
   end
 
-  def create,
+  def create
   end
 
-  def new,
+  def new
   end
 
-  def edit,
+  def edit
   end
 
-  def update,
+  def update
   end
 
-  def delete,
+  def delete
   end
 
   def destroy
   end
+
+  private
+    def t_rols_params
+      params.require(:rol).permit(:direccion_url, :li_class, :i_class, :u_class, :nombre, :descripcion, :peso, :estatus, :icon_class)
+    end
+
+    def set_t_rol
+      @rol = TRol.find(params[:id])
+    end
+
 end
