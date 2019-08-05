55cac769005d1387d0000db8d80966583d7d2853
edgkid <edglandaeta.15@gmail.com>
2019-08-05 17:12:05 +0200

    set

--- app/controllers/t_users_controller.rb
+++ app/controllers/t_users_controller.rb
@@ -1,5 +1,7 @@
 require "user.rb"
 class TUsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -15,4 +17,19 @@ class TUsersController < ApplicationController
 
   def edit
   end
+
+  def update
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:nombre, :apellido, :estado, :email, :encrypted_password)
+    end
+
 end
