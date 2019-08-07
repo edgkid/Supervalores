4e2d4a31b5b5f30d49f6142ae2f997c936f235b7
edgkid <edglandaeta.15@gmail.com>
2019-08-07 08:03:37 +0200

    ajustes varios

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
+    @usar_dataTables = true
     @users = User.all
   end
 
