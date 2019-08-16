3898f35fa848d5665832d9a407251cb34bb61e48
edgkid <edglandaeta.15@gmail.com>
2019-08-16 19:29:00 +0200

    Elemento y roles -Migracion
    Se crea migracion para asociar los roles creados a los modulos del sistema que se accede

---
+++ db/migrate/20190816172336_create_t_rols_elementos.rb
@@ -0,0 +1,14 @@
+class CreateTRolsElementos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :t_rols_elementos do |t|
+
+      t.boolean :lee, null: false
+      t.boolean :crea, null: false
+      t.boolean :edita, null: false
+      t.boolean :elimina, null: false
+
+      t.belongs_to :t_rol, foreign_key: true, null: false
+      t.belongs_to :t_elemento, foreign_key: true, null: false
+    end
+  end
+end
