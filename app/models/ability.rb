79fbc8ce93fb1c2465ecdc168a153009ffa2da55
Jesús Di Bari <iijbari@gmail.com>
2019-08-16 16:10:25 -0400

    Give all permissions to all

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,13 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+    user ||= User.new # guest user (not logged in)
+    can :manage, :all
+    # if user.admin?
+    #   can :manage, :all
+    # else
+    #   can :manage, :all
+    # end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
@@ -30,26 +31,26 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-    alias_action :create, :read, :update, :destroy, to: :crud
-    if user.estado
-      if user.present?
-          #Action to Super Admin
-          can :manage, :all if user.role == "SuperAdmin"
+    # alias_action :create, :read, :update, :destroy, to: :crud
+    # if user.estado
+    #   if user.present?
+    #       #Action to Super Admin
+    #       can :manage, :all if user.role == "SuperAdmin"
 
-          #Action to AdminCxC
-          can :crud, TCliente if user.role == "AdminCxC"
-          can :crud, TEmpresa if user.role == "AdminCxC"
-          can :crud, TEstatus if user.role == "AdminCxC"
-          can :crud, TFactura if user.role == "AdminCxC"
-          can :crud, TLeyenda if user.role == "AdminCxC"
-          can :crud, TPeriodo if user.role == "AdminCxC"
-          can :crud, TPersona if user.role == "AdminCxC"
-          can :crud, TRecargo if user.role == "AdminCxC"
-          can :crud, TResolucion if user.role == "AdminCxC"
-          can :crud, TTarifaServicio if user.role == "AdminCxC"
-          can :crud, TTipoPersona if user.role == "AdminCxC"
+    #       #Action to AdminCxC
+    #       can :crud, TCliente if user.role == "AdminCxC"
+    #       can :crud, TEmpresa if user.role == "AdminCxC"
+    #       can :crud, TEstatus if user.role == "AdminCxC"
+    #       can :crud, TFactura if user.role == "AdminCxC"
+    #       can :crud, TLeyenda if user.role == "AdminCxC"
+    #       can :crud, TPeriodo if user.role == "AdminCxC"
+    #       can :crud, TPersona if user.role == "AdminCxC"
+    #       can :crud, TRecargo if user.role == "AdminCxC"
+    #       can :crud, TResolucion if user.role == "AdminCxC"
+    #       can :crud, TTarifaServicio if user.role == "AdminCxC"
+    #       can :crud, TTipoPersona if user.role == "AdminCxC"
 
-      end
-    end
+    #   end
+    # end
   end
 end
