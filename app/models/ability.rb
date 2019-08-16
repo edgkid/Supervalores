64a37dff11a81dacc8457b62db6c76b1e6ad53a1
edgkid <edglandaeta.15@gmail.com>
2019-08-16 04:38:40 +0200

    Validar usuarios activos

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -31,23 +31,25 @@ class Ability
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
     alias_action :create, :read, :update, :destroy, to: :crud
-    if user.present?
-        #Action to Super Admin
-        can :manage, :all if user.role == "SuperAdmin"
+    if user.estado
+      if user.present?
+          #Action to Super Admin
+          can :manage, :all if user.role == "SuperAdmin"
 
-        #Action to AdminCxC
-        can :crud, TCliente if user.role == "AdminCxC"
-        can :crud, TEmpresa if user.role == "AdminCxC"
-        can :crud, TEstatus if user.role == "AdminCxC"
-        can :crud, TFactura if user.role == "AdminCxC"
-        can :crud, TLeyenda if user.role == "AdminCxC"
-        can :crud, TPeriodo if user.role == "AdminCxC"
-        can :crud, TPersona if user.role == "AdminCxC"
-        can :crud, TRecargo if user.role == "AdminCxC"
-        can :crud, TResolucion if user.role == "AdminCxC"
-        can :crud, TTarifaServicio if user.role == "AdminCxC"
-        can :crud, TTipoPersona if user.role == "AdminCxC"
+          #Action to AdminCxC
+          can :crud, TCliente if user.role == "AdminCxC"
+          can :crud, TEmpresa if user.role == "AdminCxC"
+          can :crud, TEstatus if user.role == "AdminCxC"
+          can :crud, TFactura if user.role == "AdminCxC"
+          can :crud, TLeyenda if user.role == "AdminCxC"
+          can :crud, TPeriodo if user.role == "AdminCxC"
+          can :crud, TPersona if user.role == "AdminCxC"
+          can :crud, TRecargo if user.role == "AdminCxC"
+          can :crud, TResolucion if user.role == "AdminCxC"
+          can :crud, TTarifaServicio if user.role == "AdminCxC"
+          can :crud, TTipoPersona if user.role == "AdminCxC"
 
+      end
     end
   end
 end
