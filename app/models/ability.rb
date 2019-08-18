e9e9187b539dd7c3b0cf83a56624d2bb312fdb35
edgkid <edglandaeta.15@gmail.com>
2019-08-18 13:58:12 +0200

    Ability modificados
    S cargaron todos los modelos a los roles estaticos y por defeto de administradores

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -25,7 +25,27 @@ class Ability
         can :crud, TResolucion if user.role == "AdminCxC"
         can :crud, TTarifaServicio if user.role == "AdminCxC"
         can :crud, TTipoPersona if user.role == "AdminCxC"
-
+        can :crud, TCaja if user.role == "AdminCxC"
+        can :crud, TCatalogoCuentaSub if user.role == "AdminCxC"
+        can :crud, TClienteTarifa if user.role == "AdminCxC"
+        can :crud, TCuentaFinanciera if user.role == "AdminCxC"
+        can :crud, TCuentaVentum if user.role == "AdminCxC"
+        can :crud, TEmailMasivo if user.role == "AdminCxC"
+        can :crud, TEmision if user.role == "AdminCxC"
+        can :crud, TEmpresa if user.role == "AdminCxC"
+        can :crud, TEstadoCuentaCont if user.role == "AdminCxC"
+        can :crud, TEstadoCuentum if user.role == "AdminCxC"
+        can :crud, TFacturaDetalle if user.role == "AdminCxC"
+        can :crud, TMetodoPago if user.role == "AdminCxC"
+        can :crud, TNotaCredito if user.role == "AdminCxC"
+        can :crud, TPresupuesto if user.role == "AdminCxC"
+        can :crud, TRecargoXCliente if user.role == "AdminCxC"
+        can :crud, TRecibo if user.role == "AdminCxC"
+        can :crud, TTarifaServicioGroup if user.role == "AdminCxC"
+        can :crud, TTarifa if user.role == "AdminCxC"
+        can :crud, TTipoCliente if user.role == "AdminCxC"
+        can :crud, TTipoCuenta if user.role == "AdminCxC"
+        can :crud, TTipoEmision if user.role == "AdminCxC"
        end
      end
   end
