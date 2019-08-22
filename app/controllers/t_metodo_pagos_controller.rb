5527d152d0a74208d2226d591f0b04c25b2302e9
edgkid <edglandaeta.15@gmail.com>
2019-08-22 06:27:03 +0200

    Controlador Programado

--- app/controllers/t_metodo_pagos_controller.rb
+++ app/controllers/t_metodo_pagos_controller.rb
@@ -3,18 +3,37 @@ class TMetodoPagosController < ApplicationController
   before_action :set_t_metodo_pago, only: [:edit, :update, :destroy]
 
   def index
+    @t_metodo_pago = TMetodoPago.all
   end
 
   def new
+    @t_metodo_pago = TMetodoPago.new
   end
 
   def create
+    @t_metodo_pago = TMetodoPago.new(t_metodo_pago_params)
+
+    if @t_metodo_pago.save
+      redirect_to t_metodo_pagos_index_path, notice: 'Método de pago creado correctamente.'
+    else
+      @notice = @t_metodo_pago.errors
+      render :action => "new"
+    end
   end
 
   def edit
+    @t_metodo_pago =TMetodoPago.find(params[:id])
   end
 
   def update
+    @t_metodo_pago = TMetodoPago.find(params[:id])
+
+    if TMetodoPago.update_attributes(t_metodo_pago_params)
+      redirect_to t_metodo_pagos_index_path , notice: 'Método de pago actualizado correctamente.'
+    else
+      @notice = @t_metodo_pago.errors
+      render :action => "edit"
+    end
   end
 
   private
