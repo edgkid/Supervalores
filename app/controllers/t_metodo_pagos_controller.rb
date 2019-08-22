0949c56757c657e6cdda22837bb041d46afa994c
edgkid <edglandaeta.15@gmail.com>
2019-08-22 06:10:09 +0200

    Estructura Controlador

--- app/controllers/t_metodo_pagos_controller.rb
+++ app/controllers/t_metodo_pagos_controller.rb
@@ -1,10 +1,29 @@
 class TMetodoPagosController < ApplicationController
+
+  before_action :set_t_metodo_pago, only: [:edit, :update, :destroy]
+
   def index
   end
 
   def new
   end
 
+  def create
+  end
+
   def edit
   end
+
+  def update
+  end
+
+  private
+    def t_metodo_pago_params
+      params.require(:t_metodo_pago).permit(:forma_pago, :descripcion, :minimo, :maximo, :estatus)
+    end
+
+    def set_t_metodo_pago
+      @rol = TRol.find(params[:id])
+    end
+
 end
