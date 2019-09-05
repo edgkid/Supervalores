9b91aa2e8adbbeeb81cdc4021bdce8335fb811ee
Jesús Di Bari <iijbari@gmail.com>
2019-09-05 18:49:50 -0400

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_200555) do
+ActiveRecord::Schema.define(version: 2019_09_05_002946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "crono_jobs", force: :cascade do |t|
+    t.string "job_id", null: false
+    t.text "log"
+    t.datetime "last_performed_at"
+    t.boolean "healthy"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_crono_jobs_on_job_id", unique: true
+  end
 
   create_table "t_cajas", force: :cascade do |t|
     t.float "pago_recibido", null: false
@@ -355,8 +365,6 @@ ActiveRecord::Schema.define(version: 2019_09_04_200555) do
     t.string "nombre", null: false
     t.string "apellido", null: false
     t.bigint "num_licencia", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.bigint "t_empresa_id"
     t.string "cargo"
     t.string "telefono"
