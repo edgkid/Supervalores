cbe88a8eaa318c2ca10965653c0b704ae825cdba
Tommy Joa <tjoa@siscotelcloud.com>
2019-10-11 10:44:12 -0500

    pdf route

--- app/controllers/t_facturas_controller.rb
+++ app/controllers/t_facturas_controller.rb
@@ -1,5 +1,5 @@
 class TFacturasController < ApplicationController
-  before_action :set_t_factura, only: [:edit, :update, :preview, :show, :destroy]
+  before_action :set_t_factura, only: [:edit, :update, :preview, :show, :destroy, :generar_pdf]
   load_and_authorize_resource
 
   def new
