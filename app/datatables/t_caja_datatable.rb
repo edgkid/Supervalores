3dbe656a5c0ba9eb062397b6075d71653775da54
Jesús Di Bari <iijbari@gmail.com>
2019-10-20 16:43:17 -0400

    Solve error about 'eager load' (trying to join polymorphic association tables)

--- app/datatables/t_caja_datatable.rb
+++ app/datatables/t_caja_datatable.rb
@@ -2,7 +2,8 @@ class TCajaDatatable < ApplicationDatatable
   def view_columns
     @view_columns ||= {
       id: { source: "TRecibo.id" },
-      t_cliente: { source: "TCliente.id" },
+      # t_cliente: { source: "TCliente.id" },
+      t_cliente: { source: "TEmpresa.razon_social | (TPersona.nombre & TPersona.apellido) | TOtro.razon_social" },
       t_resolucion: { source: "TResolucion.resolucion" },
       fecha_pago: { source: "TRecibo.fecha_pago" },
       pago_pendiente: { source: "TRecibo.pago_pendiente" },
@@ -13,9 +14,13 @@ class TCajaDatatable < ApplicationDatatable
 
   def data
     records.map do |record|
+      t_empresa = record.t_cliente.persona.try(:rif)            ? record.t_cliente.persona : nil
+      t_persona = record.t_cliente.persona.try(:cedula)         ? record.t_cliente.persona : nil
+      t_otro    = record.t_cliente.persona.try(:identificacion) ? record.t_cliente.persona : nil
+
       {
         id: record.id,
-        t_cliente: record.t_cliente.id,
+        t_cliente: t_empresa.razon_social || t_persona.try(:nombre_completo) || t_otro.razon_social,
         t_resolucion: record.t_factura.t_resolucion.resolucion,
         fecha_pago: record.fecha_pago,
         pago_pendiente: record.pago_pendiente,
@@ -27,15 +32,24 @@ class TCajaDatatable < ApplicationDatatable
   end
 
   def get_raw_records
-    joins = TRecibo.joins(:t_cliente, {t_factura: [:t_resolucion]}, :user)
+    # t_recibos = TRecibo.joins({ t_cliente: [t_persona: { persona_type: 't_persona' }] }, { t_factura: [:t_resolucion] }, :user)
+    # t_recibos = TRecibo.joins(:t_cliente, {t_factura: [:t_resolucion]}, :user)
+
+    t_recibos = TRecibo.joins("
+      LEFT JOIN t_clientes ON t_clientes.id = t_recibos.t_cliente_id
+      LEFT JOIN t_empresas ON t_empresas.id = t_clientes.persona_id AND t_clientes.persona_type = 'TEmpresa'
+      LEFT JOIN t_personas ON t_personas.id = t_clientes.persona_id AND t_clientes.persona_type = 'TPersona'
+      LEFT JOIN t_otros    ON t_otros.id    = t_clientes.persona_id AND t_clientes.persona_type = 'TOtro'
+    ").joins({ t_factura: :t_resolucion }, :user)
+
     if params[:from] && params[:from] != '' && params[:to] && params[:to] != ''
-      joins.where('fecha_pago BETWEEN ? AND ?', params[:from], params[:to])
+      t_recibos.where('fecha_pago BETWEEN ? AND ?', params[:from], params[:to])
     elsif params[:from] && params[:from] != ''
-      joins.where('fecha_pago >= ?', params[:from])
+      t_recibos.where('fecha_pago >= ?', params[:from])
     elsif params[:to] && params[:to] != ''
-      joins.where('fecha_pago <= ?', params[:to])
+      t_recibos.where('fecha_pago <= ?', params[:to])
     else
-      TRecibo.joins(:t_cliente, {t_factura: [:t_resolucion]}, :user)
+      t_recibos
     end
   end
 end
