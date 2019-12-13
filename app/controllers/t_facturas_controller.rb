2c5b37abaad58e637ac3f08607676be1bc8bd037
Wilson Manyoma <wmanyoma@siscotelcloud.com>
2019-12-13 18:25:35 -0500

    fix errores

--- app/controllers/t_facturas_controller.rb
+++ app/controllers/t_facturas_controller.rb
@@ -42,10 +42,21 @@ class TFacturasController < ApplicationController
       #Condicion para aplicar nota de  crédito
       if true
         @t_nota_credito = @t_factura.t_cliente.t_nota_creditos.where(status:"Sin Usar").first
-        @t_nota_credito.t_factura_id = @t_factura.id
-        @t_nota_credito.status = "Usada"
-        #@t_nota_credito.monto = Aqui debe ir el monto que se le asigna a la factura
-        @t_nota_credito.save!
+        unless @t_nota_credito.nil?
+          @t_nota_credito.t_factura_id = @t_factura.id
+          @t_nota_credito.status = "Usada"
+          if @t_factura.total_factura - @t_nota_credito.monto < 0
+              new_nota_credito = TNotaCredito.new
+              new_nota_credito.t_cliente_id = @t_nota_credito.t_cliente_id
+              new_nota_credito.t_recibo_id = @t_nota_credito.id
+              new_nota_credito.monto = @t_recibo.monto_acreditado
+              new_nota_credito.monto_original =  @t_recibo.monto_acreditado
+              new_nota_credito.status = "Sin Usar"
+              new_nota_credito.save!
+          end
+          #@t_nota_credito.monto = Aqui debe ir el monto que se le asigna a la factura
+          @t_nota_credito.save!
+        end
       end
       redirect_to new_t_factura_t_recibo_path(@t_factura), notice: 'Factura creada exitosamente.'
     else
