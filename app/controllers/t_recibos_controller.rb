1abaf0ddea976edd03c2c26844b3f92c1231e3f2
Wilson Manyoma <wmanyoma@siscotelcloud.com>
2019-12-13 22:14:29 -0500

    fixes varios

--- app/controllers/t_recibos_controller.rb
+++ app/controllers/t_recibos_controller.rb
@@ -169,12 +169,8 @@ class TRecibosController < ApplicationController
     def set_necessary_objects
       @t_recibos = @t_factura.t_recibos
       @t_nota_creditos = @t_factura.t_nota_creditos
-      monto = @t_nota_creditos.last.nil? ? 0 : @t_nota_creditos.last.monto
-      if @t_recibos.any?
-        @pending_payment = @t_recibos.last.pago_pendiente.truncate(2) - monto
-      else
-        @pending_payment = @t_factura.calculate_pending_payment.truncate(2) - monto
-      end
+      @pending_payment = @t_factura.pendiente_total.truncate(2)
+
 
       @t_resolucion = @t_factura.t_resolucion
       # @t_tarifa  = @t_resolucion.t_tipo_cliente.t_tarifa
--- app/models/t_recibo.rb
+++ app/models/t_recibo.rb
@@ -71,7 +71,7 @@ class TRecibo < ApplicationRecord
   def get_services_total(t_factura, has_no_receipts)
     ammount = has_no_receipts ? t_factura.calculate_services_total_price : t_factura.t_recibos.last.servicios_x_pagar
     credit = t_factura.t_nota_creditos.last.nil? ? 0 :  t_factura.t_nota_creditos.last.monto
-    (ammount - credit)
+    (ammount.to_f - credit)
   end
 
   def get_total_surcharge(t_factura, has_no_receipts)
