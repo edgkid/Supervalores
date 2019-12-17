9537894d0030e096b7cc7c23e0eb5398dc62df1a
Tommy Joa <tjoa@siscotelcloud.com>
2019-12-17 13:30:13 -0500

    heroku test

--- app/controllers/t_recibos_controller.rb
+++ app/controllers/t_recibos_controller.rb
@@ -141,6 +141,8 @@ class TRecibosController < ApplicationController
     
     @resoluciones = TResolucion.joins(t_facturas: [{t_factura_detalles: :t_tarifa_servicio}, :t_recibos]).includes(t_facturas: [{t_factura_detalles: :t_tarifa_servicio}, :t_recibos]).where("extract(year from Date(t_recibos.fecha_pago)) in (#{query_years.join(',')})")
 
+    @resoluciones = TResolucion.where("id < 20")
+
     @resoluciones.each do |resolucion|
       next if resolucion.t_facturas.count == 0
       resolucion.t_facturas.each do |factura|
